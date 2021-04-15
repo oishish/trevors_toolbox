56cf8bb285e0a1547c38b0014bb0c62dd0c28a21
Trevor Arp <trevorarp@gmail.com>
2021-04-15 11:25:00 -0700

    Added retrival from data vault

--- trevorarp/data.py
+++ trevorarp/data.py
@@ -3,8 +3,34 @@ A Module for data processing between from various formats
 '''
 
 import h5py
+import labrad
+
 from os.path import exists
 import numpy as np
+
+def retrievefromvault(vaultdir, filename, host='localhost', password='pass'):
+    '''
+    A tool to retrieve files from a LabRAD datavault
+
+    Args:
+        vaultdir (str) : The sub directory of the vault ot find the files in (neglecting the .dir extension)
+        filename (str) : The name of the file, neglecting the leading numbers or file extenstion, for
+            example "data1" for the file "00001 - data1.hdf" if there are files with the same name but
+            different numbers it will always retreive the first instance.
+        host (str) : The host for the labrad connection, localhost by default.
+        password (str) : The password for the labrad connection, localhost password by default.
+    '''
+    dv = labrad.connect('localhost', password='pass').data_vault
+    for dir in vaultdir.split('\\'):
+        dv.cd(dir)
+    rt, fls = dv.dir()
+    for fl in fls:
+        if fl.split(' - ',1)[1] == filename:
+            datafile = fl
+            break
+    dv.open(datafile)
+    return np.array(dv.get())
+#
 
 def datavault2numpy(filename):
     '''
