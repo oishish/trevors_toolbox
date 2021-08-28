e5cb75bd152bc786277ca9f690f083f864151300
Trevor Arp <trevorarp@ucsb.edu>
2021-08-27 18:55:45 -0700

    Added nanosquid style data vault retreival

--- trevorarp/data.py
+++ trevorarp/data.py
@@ -1,5 +1,5 @@
 '''
-A Module for data processing between from various formats
+A module for data processing between from various formats
 '''
 
 import h5py
@@ -8,9 +8,41 @@ import labrad
 from os.path import exists
 import numpy as np
 
+def get_dv_data(identifier, remote=None, subfolder=None):
+    '''
+    A function to retreive data from the datavault using a nanosquid identifier and return is as numpy arrays
+
+    Args:
+        identifier (str): The specific
+        remote (str): If not None will access data from a vault on another computer. This parameter
+            is the remote name for the labrad.connect function
+        subfolder : If not None access a subfolder within the vault. Works like an argument of the
+            datavault.cd function, i.e. takes a String or list of strings forming a path to the folder.
+    '''
+    if remote is None:
+        cxn = labrad.connect()
+    else:
+        cxn = labrad.connect(remote, password='pass')
+
+    dv = cxn.data_vault
+    if subfolder is not None:
+        dv.cd(subfolder)
+
+    drs, fls = dv.dir()
+    filename = [x for x in fls if identifier in x]
+
+    if len(filename) == 0:
+        raise IOError("Identifier " + identifier + " not found on this data vault.")
+    elif len(filename) > 1:
+        print("Warning files with duplicate identifiers detected, only the first one was retreived")
+    datafile = filename[0]
+    dv.open(datafile)
+    data = np.array(dv.get())
+    return data
+
 def retrievefromvault(vaultdir, filename, host='localhost', password='pass'):
     '''
-    A tool to retrieve files from a LabRAD datavault
+    A generic tool to retrieve files from a LabRAD datavault
 
     Args:
         vaultdir (str) : The sub directory of the vault ot find the files in (neglecting the .dir extension)
